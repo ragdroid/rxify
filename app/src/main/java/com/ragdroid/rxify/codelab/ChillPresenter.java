b7b621055464af2a0df1c5e8a988852d3f32e5b5
ragdroid <garima.my.way@gmail.com>
2017-01-20 03:44:06 +0530

    The Battle Flow

--- app/src/main/java/com/ragdroid/rxify/codelab/ChillPresenter.java
+++ app/src/main/java/com/ragdroid/rxify/codelab/ChillPresenter.java
@@ -3,14 +3,10 @@ package com.ragdroid.rxify.codelab;
 import com.ragdroid.rxify.codelab.presenter.BaseCLPresenter;
 import com.ragdroid.rxify.core.BaseSchedulerProvider;
 
-import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import io.reactivex.Observable;
-import io.reactivex.ObservableSource;
+import io.reactivex.Flowable;
 import io.reactivex.disposables.Disposable;
-import io.reactivex.functions.BiFunction;
-import io.reactivex.functions.Function;
 
 /**
  * Created by garimajain on 15/01/17.
@@ -18,7 +14,7 @@ import io.reactivex.functions.Function;
 
 public class ChillPresenter extends BaseCLPresenter<Long> implements CodeLabContract.Presenter {
 
-    public Observable<Long> inputValues = Observable.interval(1, 1, TimeUnit.MICROSECONDS);
+    public Flowable<Long> inputValues = Flowable.interval(1, 1, TimeUnit.MICROSECONDS);
 
     public ChillPresenter(BaseSchedulerProvider provider) {
         super(provider);
@@ -26,61 +22,30 @@ public class ChillPresenter extends BaseCLPresenter<Long> implements CodeLabCont
 
     //TODO Run the code.
 
-//    @Override
-//    protected Disposable getDisposable() {
-//        return inputValues
-//                .subscribeOn(provider.io())
-//                .observeOn(provider.ui())
-//                .subscribe(next, error, complete);
-//    }
-
-    //TODO Limit the number of inputs to 129
-
-//    @Override
-//    protected Disposable getDisposable() {
-//        return inputValues
-//                .take(129)
-//                .subscribeOn(provider.io())
-//                .observeOn(provider.ui())
-//                .subscribe(next, error, complete);
-//    }
-
-    //TODO Buffer 10 items and concatenate them to a String
-
-//    @Override
-//    protected Disposable getDisposable() {
-//        return inputValues
-//                .take(129)
-//                .buffer(10)
-//                .flatMap(new Function<List<Long>, ObservableSource<String>>() {
-//                    @Override
-//                    public ObservableSource<String> apply(List<Long> longs) throws Exception {
-//                        return Observable.fromIterable(longs)
-//                                .reduce("", new BiFunction<String, Long, String>() {
-//                                    @Override
-//                                    public String apply(String oldString, Long aLong) throws Exception {
-//                                        return oldString + "\n" + aLong;
-//                                    }
-//                                }).toObservable();
-//                    }
-//                })
-//                .subscribeOn(provider.io())
-//                .observeOn(provider.ui())
-//                .subscribe(next, error, complete);
-//    }
-
-//    SIRIUSLY
-
-    //TODO Use Debounce 1 Microsecond to drop some values
-
-
     @Override
     protected Disposable getDisposable() {
         return inputValues
-                .take(129)
-                .debounce(1, TimeUnit.MICROSECONDS)
                 .subscribeOn(provider.io())
                 .observeOn(provider.ui())
                 .subscribe(next, error, complete);
     }
+
+    //TODO Limit the number of inputs to 129
+
+    /** TODO Go inside FlowableInterval class to see where the Exception is thrown
+     *
+     * Flowable.interval() method is tagged as @BackpressureSupport(BackpressureKind.ERROR)
+     * ERROR :
+     * The operator will emit a MissingBackpressureException if the downstream didn't request
+     * enough or in time.
+     *
+     * Consumers should consider applying one of the {@code onBackpressureXXX} operators as well.
+     */
+
+    //TODO apply onBackpressureBuffer() strategy
+
+    //TODO apply onBackpressureDrop() strategy
+
+
+
 }
